96b84b7150fd4a95273d16efd734d3f42bcd1f21
alonic2 <farhow30@gmail.com>
2021-10-22 17:12:03 +0330

    Add All Files

---
+++ Currency_Pair.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+from forex_python.converter import get_rates
+import fire
+import pandas as pd
+
+def Pair(name):
+
+    DataT = pd.Series(get_rates(str(name)))
+    DataT.to_csv(f"{name}.csv")
+
+if __name__ == '__main__':
+    fire.Fire()
